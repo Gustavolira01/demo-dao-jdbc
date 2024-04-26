acb2fcaa25d589242086ca1615e82bcf8446bb37
Gustavo@Lira <gustavo.liras@hotmail.com>
2024-04-26 01:07:55 -0300

    Departament entity class

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Departament;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Departament departament = new Departament(1, "Books");
+		System.out.println(departament);
+
+	}
+
+}
