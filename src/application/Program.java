2db1f0ee3f0b8221485bda79c1dd3efc52457f8d
Gustavo@Lira <gustavo.liras@hotmail.com>
2024-04-27 23:36:52 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departament;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Departament departament = new Departament(1, "Books");
-		System.out.println(departament);
+		Seller seller = new Seller(1, "Gustavo Lira", "gustavo@gmail.com", new Date(), 3000.00, departament);
+		System.out.println(seller);
 
 	}
 
